aa09eabc0e7b51a95b36e2da1992255d5e756516
TomzinM <tomzinhom@gmail.com>
2026-05-14 09:27:02 -0300

    Adicionando Resto do Menu

--- main.c
+++ main.c
@@ -12,15 +12,80 @@ printf("---------------------------\n");
 printf("Escolha uma opcao: ");
 }
 
-int main(){
+void consultarSaldo(float money) {
+    printf("\nSaldo atual: R$ %.2f\n", money);
+}
 
-    exibirMenu();
+float realizarDeposito(float money) {
+    float valor;
+
+    printf("Digite o valor do deposito: ");
+    scanf("%f", &valor);
+
+    if(valor > 0) {
+        money += valor;
+        printf("Deposito realizado!\n");
+    } else {
+
+        printf("Valor invalido!\n");
+    }
+
+    return money;
+}
+
+float realizarSaque(float money) {
+
+    float valor;
+
+    printf("Digite o valor do saque: ");
+    scanf("%f", &valor);
+    if(valor > 0 && valor <= money) {
+
+        money -= valor;
+        printf("Saque realizado!\n");
+
+    } else {
+
+        printf("Saldo insuficiente ou valor invalido!\n");
+    }
+    
+    return money;
+}
+
+int main(){
+    float saldo = 0.0;
     int opcao;
+
     do {
-    exibirMenu();
-    scanf("%d", &opcao);
+
+        exibirMenu();
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+
+            case 1:
+                consultarSaldo(saldo);
+                break;
+
+            case 2:
+                saldo = realizarSaque(saldo);
+                break;
+
+            case 3:
+                saldo = realizarDeposito(saldo);
+                break;
+
+            case 0:
+                printf("Encerrando sistema...\n");
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
+        }
+
     } while(opcao != 0);
+
     printf("Obrigado por usar nosso ATM!\n");
- 
     return 0;
+
 }
