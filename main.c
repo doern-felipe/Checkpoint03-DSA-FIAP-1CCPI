884ad31fb78c5fed2834db7fe513913cbb71045d
TomzinM <tomzinhom@gmail.com>
2026-05-14 09:08:03 -0300

    Adicionado Menu

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+void exibirMenu() {
+printf("\n-------------------------\n");
+printf(" CAIXA ELETRONICO \n");
+printf("---------------------------\n");
+printf("0 - Sair\n");
+printf("1 - Consultar Saldo\n");
+printf("2 - Realizar Saque\n");
+printf("3 - Realizar Deposito\n");
+printf("---------------------------\n");
+printf("Escolha uma opcao: ");
+}
+
+int main(){
+
+    exibirMenu();
+    
+
+    return 0;
+}
